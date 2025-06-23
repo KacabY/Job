fd9e980ae8c816f3de5a8715ec70df868ac26dfc
KacabY <kacab.yuusuf@edu.orebro.se>
2025-06-23 18:43:16 +0200

    models

---
+++ jobModel.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace LJOB.Models
+{
+    public class jobModel
+    {
+        public int AId { get; set; }
+        public string? jobTitle { get; set; }
+        public string? company { get; set; }
+        public string? position { get; set; }
+        public string? location { get; set; }
+        public DateTime appliedDate { get; set; }
+        public string? status { get; set; }
+        public string? description { get; set; }
+        public string? salary { get; set; }
+        public string? applicationUrl { get; set; }
+        public string? contactPerson { get; set; }
+        public string? notes { get; set; }
+
+
+    }
+}
